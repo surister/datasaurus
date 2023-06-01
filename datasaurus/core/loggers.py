f6ad0e1e5ca74661cc21cc726c3711c5469410c0
surister <surister98@gmail.com>
2023-06-01 17:57:45 +0200

    Change loger name

--- datasaurus/core/loggers.py
+++ datasaurus/core/loggers.py
@@ -1,6 +1,5 @@
 import logging
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s')
-datasaurus_logger = logging.getLogger('Datasaurus_logger')
+datasaurus_logger = logging.getLogger('datasaurus')
 datasaurus_logger.setLevel(logging.DEBUG)
-
