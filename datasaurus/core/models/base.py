4f35b04ab378a4977448c131b0867e87ea38b33a
surister <surister98@gmail.com>
2023-06-01 17:57:23 +0200

    Allow to override default storage from model

--- datasaurus/core/models/base.py
+++ datasaurus/core/models/base.py
@@ -5,6 +5,7 @@ import polars
 from polars import DataFrame
 
 from datasaurus.core.loggers import datasaurus_logger
+from datasaurus.core.storage.base import Storage
 
 
 class Field:
@@ -54,22 +55,25 @@ class Manager:
         self.storage = getattr(meta, '__storage__')
         self.table_name = getattr(meta, '__table_name__')
 
-    def _validate_columns(self, df: polars.DataFrame):
+    def _validate_columns(self, df: polars.DataFrame, columns: list[str]):
         datasaurus_logger.debug('Validating columns')
-        if len(set(df.columns) - set(self.columns)) != 0:
-            raise Exception(f"Cannot write df since columns don't match, your dataframe's columns: {df.columns}, your model columns: {self.columns}")
+        if len(set(df.columns) - set(columns)) != 0:
+            raise Exception(
+                f"Cannot write df since columns don't match, your dataframe's columns: {df.columns}, your model columns: {self.columns}")
 
     def read_df(self, columns):
         return self.storage.from_env.read_file(self.table_name, columns)
 
-    def write_df(self, df: polars.DataFrame):
+    def write_df(self, df: polars.DataFrame, storage: Storage = None):
+        storage = storage or self.storage.from_env
         if getattr(self.meta, '__auto_select__', False):
             df = df.select(self.columns)
         self._validate_columns(df)
-        return self.storage.from_env.write_file(self.table_name, df)
+        return storage.write_file(self.table_name, df)
 
-    def data_exists(self) -> bool:
-        return self.storage.from_env.file_exists(self.table_name)
+    def data_exists(self, storage=None) -> bool:
+        storage = storage or self.storage.from_env
+        return storage.file_exists(self.table_name)
 
 
 class ModelBase(type):
@@ -103,20 +107,19 @@ class ModelBase(type):
     def calculate_data(cls):
         raise NotImplementedError()
 
-    def exists(cls) -> bool:
+    def exists(cls, storage: Storage = None) -> bool:
         """
-        Return whether the source data to populate the df exists, can be used to check if it's
-        necessary to calculate the data (for models that are derived of others), or to sanity check
-        read operations.
+        Return whether the source data to populate the df exists in the given storage,
+        if no storage is provided the default one will be used (Defined in Meta)
         """
-        return cls._manager.data_exists()
+        return cls._manager.data_exists(storage)
 
-    def ensure_exists(cls):
+    def ensure_exists(cls, read_from=None, write_to=None):
         datasaurus_logger.debug(f'Making sure that {cls} exists')
-        if cls._should_be_recalculated or not cls.exists():
+        if cls._should_be_recalculated or not cls.exists(storage=read_from):
             datasaurus_logger.debug(
                 f'The data does not exist or __recalculate__ is set to True, recalculating data..')
-            cls._manager.write_df(df=cls.calculate_data(cls))
+            cls._manager.write_df(df=cls.calculate_data(cls), storage=write_to)
 
 
 class Model(metaclass=ModelBase):
