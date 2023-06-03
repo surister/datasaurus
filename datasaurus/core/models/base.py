a1833a2b75a9441d3d8a1c55e9f8ce0197e178b1
surister <surister98@gmail.com>
2023-06-03 14:21:19 +0200

    Refactor basemodel

--- datasaurus/core/models/base.py
+++ datasaurus/core/models/base.py
@@ -5,7 +5,7 @@ import polars
 from polars import DataFrame
 
 from datasaurus.core.loggers import datasaurus_logger
-from datasaurus.core.storage.base import Storage
+from datasaurus.core.storage.base import Storage, StorageGroup
 from datasaurus.core.storage.fields import Field
 
 NO_DATA = type('NO_DATA', (), {})
@@ -24,45 +24,8 @@ class lazy_func:
         return self.func()
 
 
-class Manager:
-    """
-    Manages DF operations for the Model.
-    """
-
-    def __init__(self, meta):
-        self.meta = meta
-        self.storage = getattr(meta, '__storage__')
-        self.table_name = getattr(meta, '__table_name__')
-
-    def _validate_columns(self, df: polars.DataFrame, columns: list[str]):
-        datasaurus_logger.debug('Validating columns')
-        if len(set(df.columns) - set(columns)) != 0:
-            raise Exception(
-                f"Cannot use the Dataframe since columns don't match, dataframe's columns: {df.columns}, model's columns: {columns}")
-
-    def read_df(self, columns) -> polars.DataFrame | NO_DATA:
-        try:
-            self.storage.from_env.read_file(self.table_name, columns)
-        except RuntimeError as e:
-            if 'no such table' in str(e):
-                return NO_DATA
-            else:
-                raise e
-
-    def write_df(self, df: polars.DataFrame, storage: Storage = None) -> None:
-        storage = storage or self.storage.from_env
-        if getattr(self.meta, '__auto_select__', False):
-            df = df.select(self.columns)
-        return storage.write_file(self.table_name, df)
-
-    def data_exists(self, storage=None) -> bool:
-        storage = storage or self.storage.from_env
-        return storage.file_exists(self.table_name)
-
-
 class ModelBase(type):
     df: DataFrame
-    _manager: Manager
     _data_from_cls: Optional[
         dict] = None  # This is where the data from cls.from_dict will be stored temporarily
 
@@ -81,60 +44,77 @@ class ModelBase(type):
 
     def _prepare(cls):
         meta = getattr(cls, 'Meta', None)
-        manager = Manager(meta=meta)
+        setattr(cls, '_meta', meta)
         setattr(cls, '_should_be_recalculated', getattr(meta, '__recalculate__', False))
-        setattr(cls, '_manager', manager)
         setattr(cls, 'df', lazy_func(cls._get_df))
 
     @property
     def columns(cls):
-        return [str(field) for field in cls.__dict__.values() if
-                isinstance(field, Field)]
+        return [str(column) for column in cls.__dict__.values() if
+                isinstance(column, Field)]
 
-    def calculate_data(cls):
-        raise NotImplementedError()
-
-    def exists(cls, storage: Storage = None) -> bool:
+    def _get_storage_or_default(cls, storage: Optional[Storage | StorageGroup]) -> Storage:
         """
-        Return whether the source data to populate the df exists in the given storage,
-        if no storage is provided the default one will be used (Defined in Meta)
+        Used to get either the correct Storage instance or the default Meta storage.
         """
-        return cls._manager.data_exists(storage)
+        storage = storage or cls._meta.__storage__
+        if not isinstance(storage, Storage):
+            if  StorageGroup in storage.__bases__ and len(storage.__bases__) == 1:
+                # If we use Model.save(to=Storage) instead of
+                # using Storage.from_env, or Storage.environment
+                # directly when passing the storage, we'll receive a StorageGroup instead
+                # of a storage, by default we'll just take it from the environment.
+                storage = storage.from_env
+        return storage
 
-    def save(cls, to=None):
-        datasaurus_logger.debug(f'Writing {cls}')
-        df = cls.df
-        cls._manager._validate_columns(df, cls.columns)
-        cls._manager.write_df(df=df, storage=to)
+    def _create_df(cls, using: Optional[Storage]) -> DataFrame:
+        """
+        Does the heavy lifting of creating the Dataframe from the right data source, depending on
+        opts (meta), the order of priority is as follows:
+
+        1. Data from constructor - Model.from_dict({'column1': [1,2,3})
+        2. Data from calculation - Model.calculate_data()
+        3. Data from Storage - Storage.from_env
+        """
+        if cls._data_from_cls is not None:
+            df = polars.DataFrame(cls._data_from_cls)
+            del cls._data_from_cls
+            cls._data_from_cls = None
+
+        elif cls._should_be_recalculated:
+            df = cls.calculate_data(cls)
+
+        else:
+            using = cls._get_storage_or_default(using)
+            df = using.read_file(cls._meta.__table_name__, cls.columns)
+        return df
+
+    def _get_df(cls, storage: Optional[Storage] = None):
+        df = cls._create_df(using=storage)
+
+        if getattr(cls._meta, '__auto_select__', False):
+            df = df.select(cls.columns)
+
+        columns_from_df = frozenset(df.columns)
+        missing_columns = columns_from_df.difference(cls.columns)
+
+        if missing_columns:
+            raise ValueError(
+                f"Dataframe columns do not match. df.columns: {df.columns}, models: {cls.columns}"
+            )
+        return df
 
     def from_dict(cls, d: dict):
         setattr(cls, '_data_from_cls', d)
         return cls
 
-    def _get_df(cls):
-        def inner_get_df():
-            """
-            1. Data from constructor
-            2. Data from calculation
-            3. Data from Storage
-            """
-            if cls._data_from_cls:
-                df = polars.DataFrame(cls._data_from_cls)
-                del cls._data_from_cls
-                cls._data_from_cls = None
-                return df
-
-            if cls._should_be_recalculated:
-                df = cls.calculate_data()
-                return df
-
-            df = cls._manager.read_df(cls.columns)
-            return df
-
-        df = inner_get_df()
-        cls._manager._validate_columns(df, cls.columns)
-        return df
-
 
 class Model(metaclass=ModelBase):
-    pass
+    def calculate_data(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def save(cls, to: 'Storage' = None):
+        storage = cls._get_storage_or_default(to)
+        df = cls._get_df(storage)
+        storage.write_file(df, cls._meta.__table_name__)
