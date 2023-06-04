7134c56ad8e69fb4d391beecfeb34a2af4d20ff0
surister <surister98@gmail.com>
2023-06-04 17:35:59 +0200

    Implement read for localmixin

--- datasaurus/core/storage/mixins.py
+++ datasaurus/core/storage/mixins.py
@@ -1,9 +1,10 @@
+import contextlib
 import pathlib
 
 import polars as pl
 
 from datasaurus.core.loggers import datasaurus_logger
-from datasaurus.core.models import LOCAL_FORMAT
+from datasaurus.core.models import FileFormat
 
 
 def list_to_sql_columns(l: list[str]) -> str:
@@ -47,18 +48,23 @@ class SQLStorageOperationsMixin:
 
 
 class LocalStorageOperationsMixin:
-    supported_formats = LOCAL_FORMAT
+    supported_formats = FileFormat
     needs_format = True
 
     def file_exists(self, file_name) -> bool:
         return pathlib.Path(self.get_uri()).exists()
 
-    def write_file(self, data: pl.DataFrame, file_name: str,
-                   format: LOCAL_FORMAT, **kwargs):
-        file_name = kwargs.get('overriden_filename') or file_name
+    def write_file(self, data: pl.DataFrame, file_name: str, format: FileFormat, **kwargs):
+        file_name = kwargs.pop('overriden_filename') if hasattr(kwargs,
+                                                                ' overriden_filename') else None or file_name
         full_path = f'{self.path}{file_name}.{format.name}' if format else file_name
         _write_func = getattr(data, f'write_{format.name}')
-        return _write_func(full_path)
+        return _write_func(full_path, **kwargs)
 
-    def read_file(self, file_name, format: LOCAL_FORMAT):
-        return pathlib.Path(self.get_uri()).read_text()
+    def read_file(self, file_name, columns, format: FileFormat = None, **kwargs):
+        srcdir = pathlib.Path(self.path)
+        full_path = (srcdir / file_name).with_suffix(f'.{format.name}')
+        if not full_path.exists():
+            raise ValueError(f"Cannot find '{full_path}'")
+        _read_func = getattr(pl, f'read_{format.name}')
+        return _read_func(full_path)
