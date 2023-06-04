32255e9f89a13a6cd0958a2fc858e2c0abd2234b
surister <surister98@gmail.com>
2023-06-04 17:36:19 +0200

    Rename models with latest changes

--- examples/proj1/models.py
+++ examples/proj1/models.py
@@ -1,5 +1,8 @@
-from datasaurus.core.models.base import Model, IntegerField, StringField
+from datasaurus.core.models import DataFormat, FileFormat
+from datasaurus.core.models.base import Model
+from datasaurus.core.storage.fields import StringField, IntegerField
 from examples.proj1.settings import ProfilesData
+import polars as pl
 
 
 class ProfileModel(Model):
@@ -12,20 +15,30 @@ class ProfileModel(Model):
     birthdate = StringField()
 
     class Meta:
-        __storage__ = ProfilesData
-        __table_name__ = 'PROFILE'
+        storage = ProfilesData
+        table_name = 'PROFILE'
 
 
 class FemaleProfiles(Model):
-    id = IntegerField(auto=True)
+    id = IntegerField()
     profile_id = IntegerField(unique=True)
     mail = StringField()
 
     def calculate_data(self):
-        return ProfileModel.df.filter(ProfileModel.sex == 'F')
+        return (
+            ProfileModel.df
+            .filter(ProfileModel.sex == 'F')
+            .with_row_count('new_id')
+            .with_columns(
+                pl.col('new_id')
+            )
+            .with_columns(
+                pl.col('id').alias('profile_id')
+            )
+        )
 
     class Meta:
-        __recalculate__ = True
-        __from__ = ProfileModel
-        __storage__ = ProfilesData
-        __table_name__ = 'PROFILE_FEMALES'
+        auto_select = True
+        storage = ProfilesData
+        table_name = 'PROFILE_FEMALES'
+        format = FileFormat.JSON
