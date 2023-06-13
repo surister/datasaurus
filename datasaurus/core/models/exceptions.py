830041582cd6236ddd1f6b92494c1f30a374c1fd
surister <surister98@gmail.com>
2023-06-13 20:23:21 +0200

    Add FieldNotExistsError exception

--- datasaurus/core/models/exceptions.py
+++ datasaurus/core/models/exceptions.py
@@ -8,3 +8,7 @@ class FormatNotSupportedByModelError(Exception):
 
 class FormatNeededError(Exception):
     """Raise when the Model always needs a format and it was not provided"""
+
+
+class FieldNotExistsError(Exception):
+    """Raises when field does not exist in Model and its being provided"""
