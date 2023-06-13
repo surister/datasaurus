b649da8f07732be52d8ad6bdfa427408d4ffb123
surister <surister98@gmail.com>
2023-06-13 20:23:35 +0200

    Add contest with a dummy model fixture

---
+++ tests/core/models/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from datasaurus.core import models
+from datasaurus.core.storage.fields import StringField
+
+
+@pytest.fixture
+def dummy_model_class():
+    class DummyModel(models.Model):
+        field = StringField()
+
+        class Meta:
+            pass
+
+    return DummyModel
