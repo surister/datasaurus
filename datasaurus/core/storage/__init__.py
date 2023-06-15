23bc15f6502afcf9b66d25fbd4959359d9d8e19a
surister <surister98@gmail.com>
2023-06-15 19:03:52 +0200

    Change imports to full

--- datasaurus/core/storage/__init__.py
+++ datasaurus/core/storage/__init__.py
@@ -1,5 +1,5 @@
-from .storage import LocalStorage
-from .base import StorageGroup
+from datasaurus.core.storage.storage import LocalStorage
+from datasaurus.core.storage.base import StorageGroup
 
 
 __all__ = ['LocalStorage', 'StorageGroup']
