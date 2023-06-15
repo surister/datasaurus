b4b9eb423b0d85750dbc72bf5baeba40987766ad
surister <surister98@gmail.com>
2023-06-15 19:46:27 +0200

    Move fields from storage to models.py

--- datasaurus/core/models/fields.py
+++ datasaurus/core/models/fields.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
+
 class Field:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, column_name: Optional[str] = None, *args, **kwargs):
+        self.column_name = column_name
         self._value = None
 
     def __set_name__(self, owner, name):
-        self.name = name
+        self.name = self.column_name or name
 
     def __set__(self, instance, value):
         self._value = value
