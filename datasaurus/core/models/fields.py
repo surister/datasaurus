6852e613ee12d6b4560196e253a23c7b93bbbb09
surister <surister98@gmail.com>
2023-06-16 21:55:14 +0200

    Minor refactor (add type hints, imports and abc)

--- datasaurus/core/models/fields.py
+++ datasaurus/core/models/fields.py
@@ -1,9 +1,11 @@
+from collections.abc import Collection
 from typing import Optional
 
 
 class Field:
     def __init__(self, column_name: Optional[str] = None, *args, **kwargs):
         self.column_name = column_name
+        self.name = None
         self._value = None
 
     def __set_name__(self, owner, name):
@@ -22,13 +24,13 @@ class Field:
         return self.column_name or self.name
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
     def __repr__(self):
         return f'{self.__class__.__qualname__}<{self.name}>'
 
 
-class Fields:
+class Fields(Collection):
     """Helps to control a group of fields, 1 Fields per Model obj"""
 
     def __init__(self, initial_fields: list[Field] = None):
@@ -49,14 +51,14 @@ class Fields:
     def __contains__(self, item):
         return item.name in self.get_model_columns()
 
-    def extend(self, fields: 'Fields'):
-        self._fields.extend(fields._fields)
+    def extend(self, other: 'Fields') -> None:
+        self._fields.extend(other._fields)
 
-    def get_df_columns(self):
+    def get_df_columns(self) -> list:
         """Get a list of the columns that will be used on the df write/read operations"""
         return [field.get_column_name() for field in self._fields]
 
-    def get_model_columns(self):
+    def get_model_columns(self) -> list:
         """Get the list of columns as defined in the Model"""
         return [field.name for field in self._fields]
 
