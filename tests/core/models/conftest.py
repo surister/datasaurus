a9e86f1eda11a471cb541daf322daeca09383c40
surister <surister98@gmail.com>
2023-06-25 18:00:39 +0200

    Add tests for dataframe creation from storage

--- tests/core/models/conftest.py
+++ tests/core/models/conftest.py
@@ -1,7 +1,14 @@
+import json
+import pathlib
+import uuid
+
 import pytest
 
 from datasaurus.core import models
-from datasaurus.core.models.columns import StringColumn
+from datasaurus.core.models import Model
+from datasaurus.core.models.columns import StringColumn, IntegerColumn
+from datasaurus.core.storage import StorageGroup, LocalStorage
+from datasaurus.core.storage.format import FileFormat
 
 
 @pytest.fixture
@@ -13,3 +20,34 @@ def dummy_model_class():
             pass
 
     return DummyModel
+
+
+@pytest.fixture
+def model_with_local_data():
+    tmp_dir = str(uuid.uuid4())[:4]
+    file_path = f'/tmp/rand_{tmp_dir}'
+    pathlib.Path(file_path).mkdir()
+    with open(f'{file_path}/test_model.json', 'w') as f:
+        f.write(
+            json.dumps(
+                {
+                    "columns": [
+                        {"name": "col2", "datatype": "Int64", "values": [1, 2, 3, 4]},
+                        {"name": "col1", "datatype": "Utf8", "values": ['a', 'b', 'c', 'd']}
+                    ]
+                }
+            )
+        )
+
+    class TestStorage(StorageGroup):
+        local = LocalStorage(path=file_path)
+
+    class TestModel(Model):
+        col1 = StringColumn()
+        col2 = IntegerColumn()
+
+        class Meta:
+            table_name = 'test_model'
+            storage = TestStorage
+
+    return TestModel
---
+++ tests/core/models/test_df_creation.py
@@ -0,0 +1,66 @@
+import copy
+
+import pytest
+
+from datasaurus.core.models.exceptions import FormatNeededError
+from datasaurus.core.storage.format import FileFormat
+
+"""
+The Model can create the dataframe from three different sources:
+        1. Data from constructor - Model.from_dict({'column1': [1,2,3]})
+        2. Data from calculation - Model.calculate_data()
+        3. Data from Storage - Storage
+"""
+
+
+def test_df_creation_from_storage(model_with_local_data):
+    """
+    3. Data from Storage - Storage
+    """
+    import os
+    os.environ['DATASAURUS_ENVIRONMENT'] = 'local'
+
+    model = model_with_local_data
+    default_meta = copy.copy(model_with_local_data._meta)
+
+    def reset_meta(model):
+        model._meta = copy.copy(default_meta)
+
+    # Case 1: no format is given and cannot be inferred from table_name.
+    model._meta.format = None
+
+    with pytest.raises(FormatNeededError):
+        model.df
+
+    reset_meta(model)
+
+    # Case 2: no format is given and can be inferred from table_name.
+    model._meta.table_name += '.json'
+    model.df
+
+    reset_meta(model)
+
+    # Case 3: no format is given, can be inferred from table_name, but it does not exist.
+    model._meta.table_name += '.csv'
+    with pytest.raises(ValueError):
+        model.df
+
+    reset_meta(model)
+
+    # Case 4: format is given and can be inferred from table_name.
+    model._meta.format = FileFormat.JSON
+    model._meta.table_name += '.json'
+    model.df
+
+    reset_meta(model)
+
+    # Case 5: format is given, but table name does not exist.
+    model._meta.format = FileFormat.JSON
+    model._meta.table_name += '.csv'
+    model.df
+    # Make sense that it works since read_json('file.csv') doesn't really care
+    # about the suffix, remember that they are just information and not actual
+    # indicator of the internal file format.
+    reset_meta(model)
+
+# Todo add more cases for other mixins
