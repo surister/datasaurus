fc3e0f7ef15d58b370bc5df6a7d3699818e9ecf6
surister <surister98@gmail.com>
2023-07-05 22:03:00 +0200

    Make File related classes importable from storage instead of models

--- datasaurus/core/models/__init__.py
+++ datasaurus/core/models/__init__.py
@@ -1,5 +1,4 @@
 from datasaurus.core.models.base import Model
-from datasaurus.core.storage.format import FileFormat, FormatNotSet, DataFormat
 from datasaurus.core.models.exceptions import MissingMetaError
 
-__all__ = ['Model', 'FileFormat', 'FormatNotSet', 'DataFormat', 'MissingMetaError']
+__all__ = ['Model', 'MissingMetaError']
--- datasaurus/core/storage/__init__.py
+++ datasaurus/core/storage/__init__.py
@@ -1,5 +1,5 @@
 from datasaurus.core.storage.storage import LocalStorage
 from datasaurus.core.storage.base import StorageGroup
+from datasaurus.core.storage.format import FileFormat, FormatNotSet, DataFormat
 
-
-__all__ = ['LocalStorage', 'StorageGroup']
+__all__ = ['LocalStorage', 'StorageGroup', 'FileFormat', 'FormatNotSet', 'DataFormat', ]
