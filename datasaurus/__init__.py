753eb999252dd811302a3afd1bd45cd5a83d01c0
surister <surister98@gmail.com>
2023-07-05 22:03:15 +0200

    Add set_global_env utils

--- datasaurus/__init__.py
+++ datasaurus/__init__.py
@@ -1,3 +1,10 @@
+import os
+
+
 class classproperty(property):
     def __get__(self, instance, owner):
         return self.fget(owner)
+
+
+def set_global_env(environment_name: str) -> None:
+    os.environ['DATASAURUS_ENVIRONMENT'] = environment_name
